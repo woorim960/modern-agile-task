b8cb7ecd2b0d343f03e0fe8280504a6741c28a25
박우림 <woorimprog@gmail.com>
2021-02-26 03:37:49 +0900

    투두리스트 구현 시험의 초안 구현

---
+++ app/src/public/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const name = document.querySelector("#name");
+const description = document.querySelector("#description");
+const btn = document.querySelector("button");
+const table = document.querySelector("table");
