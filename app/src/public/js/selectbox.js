b36c061085d8afad039a4e1e0890a010a8696333
박우림 <woorimprog@gmail.com>
2021-02-26 21:24:00 +0900

    select box 문제 답안 구현

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const selectBox = document.getElementsByName("selectbox")[0];
+const submit = document.querySelector("#submit_btn");
+const result = document.querySelector("#result");
+console.log(selectBox.options[selectBox.selectedIndex]);
+submit.addEventListener("click", selectHandler);
+
+function selectHandler() {
+  for (let selBox of selectBox) {
+    if (selBox.selected) return requestHTTP([selBox.value]);
+  }
+}
+
+function requestHTTP(selectedBox) {
+  const req = {
+    answers: selectedBox,
+  };
+
+  fetch("/api/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) result.innerHTML = "결과: 정답입니다.";
+      else result.innerHTML = "결과: 오답입니다.";
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
