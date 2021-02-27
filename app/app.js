542e8974f01ea92b6d3972464133b21b33b24c73
박우림 <woorimprog@gmail.com>
2021-02-28 03:15:42 +0900

    개발 문제 4개 구현 및 답안 포함

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const favicon = require("serve-favicon");
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ dotenv.config();
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(favicon(`${__dirname}/src/public/images/wooahan-logo.ico`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const selectBox = document.getElementsByName("selectbox")[0];
+const submit = document.querySelector("#submit-btn");
+const result = document.querySelector("#result");
+
+submit.addEventListener("click", selectHandler);
+
+function selectHandler() {
+  for (let selBox of selectBox) {
+    if (selBox.selected) return requestHTTP(selBox.value);
+  }
+}
+
+function requestHTTP(selectBox) {
+  const req = {
+    answer: selectBox,
+  };
+
+  fetch("/api/selectbox/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) result.innerHTML = "정답";
+      else result.innerHTML = "오답";
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
