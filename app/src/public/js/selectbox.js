6d5d31abe10c7515ca17b61caf84d47bb43bc252
JeonMinJae <ssi02014@naver.com>
2021-03-01 10:50:58 +0900

    Add(minjae/selectBox): Select Box Task

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,48 @@
 "use strict";
+
+const selectBox = document.querySelector('select');
+const submitBtn = document.querySelector('#submit-btn');
+const resultSpan = document.querySelector('#result');
+
+const URI = '/api/selectbox/submit';
+
+//fetch
+// submitBtn.addEventListener('click', () => {
+//     const value = {
+//         answer: selectBox.value,
+//     }
+
+//     fetch(URI, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json' 
+//         },
+//         body: JSON.stringify(value),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data) resultSpan.innerHTML = '정답';
+//         else resultSpan.innerHTML = '오답';
+//     });
+// });
+
+//async/await 
+submitBtn.addEventListener('click', actionSumbit);
+
+async function actionSumbit() {
+    const value = {
+        answer: selectBox.value,
+    }
+
+    const data = await fetch(URI, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(value),
+        })
+        .then(response => response.json());
+    
+        if (data) resultSpan.innerHTML = '정답';
+        else resultSpan.innerHTML = '오답';
+}
