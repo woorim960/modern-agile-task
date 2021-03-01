fdf27ed78b3308bccd8cc3178ea2434aaa3364f0
JeonSoongu <soonguprog@gmail.com>
2021-03-01 11:59:20 +0900

    셀렉트박스 과제 수행

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const select = document.getElementsByName("selectbox")[0];
+const submitBtn = document.querySelector("#submit-btn");
+const result = document.querySelector("#result");
+
+submitBtn.addEventListener("click", choice);
+
+function choice() {
+    const req = {};
+
+    for (let el of select.options) {
+        if (el.selected) {
+            req.answer = el.value;
+        }
+    }
+
+    fetch("/api/selectbox/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(req)
+    })
+    .then((res) => res.text())
+    .then((res) => {
+        if (res) {
+            answer(res);
+        }
+    })
+    .catch((err) => {
+        throw err;
+    });
+
+    function answer(res) {
+        if (res === "true") {
+            result.innerHTML = "정답";
+        } else {
+            result.innerHTML = "오답";
+        }
+    }
+}
