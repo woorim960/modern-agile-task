098b96b2fe6bd2576481b6fb2b6e4691b4c290a6
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-01 15:33:34 +0900

    Add(jiwon/selectbox): selectbox 구현

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,38 @@
 "use strict";
+
+"use strict";
+
+const options = document.querySelector("#selectbox"),
+  button = document.querySelector("#submit-btn"),
+  result = document.querySelector("#result");
+
+function run() {
+  button.addEventListener("click", choice);
+}
+
+const choice = () => {
+  const data = { answer: options.value };
+
+  const post = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const requestSelect = (post) => {
+    fetch("/api/selectbox/submit", post)
+      .then((res) => res.json())
+      .then((res) => resultSelect(res));
+  };
+
+  const resultSelect = (res) => {
+    if (res) return (result.innerText = "정답");
+    result.innerText = "오답";
+  };
+
+  requestSelect(post);
+};
+
+run();
