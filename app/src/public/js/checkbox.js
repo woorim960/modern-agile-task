bc0292f47b79366126a47216603cbfb4910b2270
JeonSoongu <soonguprog@gmail.com>
2021-03-02 11:51:56 +0900

    체크박스 과제 수행

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,44 @@
 "use strict";
+
+const checkbox = document.querySelectorAll("#checkbox input");
+const btn = document.querySelector("#submit_btn");
+const result = document.querySelector("#result");
+
+btn.addEventListener("click", check);
+
+function check() {
+    let box = [];
+
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            box.push(checkbox[i].value);
+        }
+    }
+
+    const req = {
+        answers : box
+    }
+
+    fetch("/api/checkbox/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(req)
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        answer(res);
+    })
+    .catch((err) => {
+        throw err;
+    })
+
+    function answer(res) {
+        if(res) {
+            result.innerHTML = "정답";
+        } else {
+            result.innerHTML = "오답";
+        }
+    }
+}
