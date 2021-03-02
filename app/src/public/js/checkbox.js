c5595900e36915db83db54f39dd62418bfac18e3
JeonMinJae <ssi02014@naver.com>
2021-03-02 11:19:16 +0900

    Add(minjae/checkbox): Checkbox Task

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const checkBoxInputs = document.querySelectorAll('#checkbox label input');
+const submitBtn = document.querySelector('#submit_btn');
+const resultSpan = document.querySelector('#result');
+
+const URI = '/api/checkbox/submit';
+
+submitBtn.addEventListener('click', () => {
+    const value = {
+        answers: [],
+    };
+    const { answers } = value;
+
+    checkBoxInputs.forEach(input => {
+        if (input.checked) answers.push(input.value);
+    });
+
+    console.log(value);
+
+    fetch(URI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(value),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data) resultSpan.innerHTML = '정답';
+        else resultSpan.innerHTML = '오답';
+    });
+})
