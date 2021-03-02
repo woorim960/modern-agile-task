a720b0ad38ed2be825f39956f5452d137b815d3a
jsj <tw450497@gmail.com>
2021-03-02 12:04:51 +0900

    checkbox

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const checkbox = document.querySelectorAll("#checkbox label input");
+const submit = document.querySelector("#submit_btn");
+const result = document.getElementById("result");
+
+submit.addEventListener("click", output);
+
+function output() {
+    const req = {
+      answers : []
+    };
+    
+    for (let i=0; i < checkbox.length; i++) {
+      if (checkbox[i].checked == true) {
+          let a = checkbox[i].value;
+          req.answers.push(a)
+      }
+    }
+  
+    
+
+    fetch("/api/checkbox/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          result.innerHTML = `정답`;
+        } else {
+           result.innerHTML = `오답`;
+        }
+      })
+}
--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const target = document.getElementsByName("selectbox")[0];
+const submit = document.querySelector("#submit-btn");
+const result = document.getElementById("result");
+// function selectBox() {
+//     if (target.options[target.selectedIndex].value === "개발") document.querySelector('#result').innerHTML='정답';
+//     else document.querySelector('#result').innerHTML='오답';
+// }
+
+// submit.onclick = selectBox;
+
+submit.addEventListener("click", output);
+
+function output() {
+    console.log(target.value)
+    const req = {
+        answer : target.value
+    };
+
+
+    fetch("/api/selectbox/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          result.innerHTML = `정답`;
+        } else {
+           result.innerHTML = `오답`;
+        }
+      })
+  } 
