6f38b2111abec239469966a22b243e0af87e8ec0
jsj <tw450497@gmail.com>
2021-03-03 11:38:50 +0900

    login

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -1,22 +1,21 @@
 "use strict";
 
-
 const id = document.querySelector("#id");
 const submit = document.querySelector("#login");
-const password = document.getElementById("password");
+const password = document.querySelector("#password");
 const confirms = document.querySelector("#confirm");
+const result = document.getElementById("result");
 
 confirms.addEventListener("click", check);
 submit.addEventListener("click", output);
 
+let code = "";
 
 function output() {
-    
     const req = {
-        id: id.value,
-        password: password.value,
-    }
-    console.log(req)
+      id: id.value,
+      password: password.value,
+    };
 
     fetch("/api/login", {
       method: "POST",
@@ -27,17 +26,26 @@ function output() {
     })
       .then((res) => res.json())
       .then((res) => {
-        if (res) {
-            alert("코드가 발급되었습니다.")
+        if (res.msg === "success") {
+          alert("코드가 발급되었습니다.")
+          code = res.code;
         } else {
-            alert("아이디와 패스워드를 제대로 입력해 주십시오")
+          alert(res.msg)
         }
       })
   } 
 
   function check() {
-        // 코드를 가지고있다면 서버로 보낸다
-        
-        // 가지고있지않다면
-        alert("로그인을 먼저 시도하십시오")
+    fetch(`/api/check?code=${code}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.msg !== "success") {
+          alert(res.msg)
+        } else {
+          result.innerHTML = `${res.user.name}님 환영합니다.`
+        }
+      })
   }
+
+
+  //null이니 body를 지우는?
