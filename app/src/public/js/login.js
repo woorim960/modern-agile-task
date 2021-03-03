e33b48257369a8a19a1a47913435054e890430a9
JeongWonji <wonjiprog@gmail.com>
2021-03-03 11:39:47 +0900

    implement(wonji):login 문제풀이:

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -1,1 +1,65 @@
-"use strict";
+'use strict'
+
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("#login"),
+    confirmBtn = document.querySelector("#confirm"),
+    result = document.querySelector("#result");
+let code;
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    
+    const req = {
+      id: id.value,
+      password: password.value,
+    };
+
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.msg === 'success') {
+            console.log(res);
+            code = res.code;
+            name = res.name;
+            console.log(code);
+            console.log(name);
+          alert('코드가 발급되었습니다.')
+        } else {
+           alert(res.msg)
+        }
+      })
+  }
+
+
+confirmBtn.addEventListener("click", check);
+
+function check() {
+
+    const req = {
+        msg: code,
+      };
+  
+      fetch("/api/check?code="+code, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (code) {
+              console.log(res)
+            result.innerHTML = `${res.user.name}님 환영합니다`;
+          } else {
+             alert(res.msg)
+          }
+        })
+}
