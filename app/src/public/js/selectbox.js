41327dffe50c975f51eae9941d9b5ddcc3c54ecb
rugaheedori <qufslarkgml@gmail.com>
2021-05-17 17:25:33 +0900

    gahee selectbox

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,17 @@
-"use strict";
+"use strict"
+const selected = document.getElementsByName("selectbox")[0];
+const button = document.getElementById("submit-btn");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+    fetch("/api/selectbox/submit", {
+        method : "POST",
+        headers : {"Content-Type" : "application/json; charset = utf-8"},
+        body : JSON.stringify({
+            answer : selected.value        
+        })
+    })
+    .then((response) => response.json())
+    .then((isAnswer) => result.innerHTML = isAnswer ? "정답" : "오답");
+}); 
+
