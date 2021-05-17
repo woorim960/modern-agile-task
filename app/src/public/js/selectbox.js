22d11ee19baace1fc0be321a65e3d2e7299a920c
김지수 <kimji@MacBook-Pro.local>
2021-05-17 16:21:13 +0900

    kimijsu: select box 구현

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,20 @@
-"use strict";
+const selectList = document.getElementsByName("selectbox")[0];
+const submitBtn = document.getElementById("submit-btn");
+const submitResult = document.getElementById("result");
+
+submitBtn.addEventListener("click", () => {
+    fetch('/api/selectbox/submit', {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            answer: selectList.value
+        }),
+    })
+    .then(res => res.json())
+    .then(isAnswer => {
+        if (isAnswer) submitResult.innerHTML = "정답";
+        else submitResult.innerHTML = "오답";
+    })
+});
