938ddf6592d9c1c7407927bab9342eadfc099f90
유준상 <junsang@yujunsangs-MacBook-Pro.local>
2021-05-17 17:03:55 +0900

    유준상 selectbox 과제

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const selectBox = document.getElementsByName('selectbox')[0];
+const submitButton = document.getElementById('submit-btn');
+const resultSpan = document.getElementById('result');
+
+submitButton.onclick = async () => {
+  const response = await fetch('/api/selectbox/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({
+      answer: selectBox.value
+    })
+  });
+  const result = await response.json();
+  resultSpan.textContent = (result) ? '정답' : '오답';
+};
