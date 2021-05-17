4e3acc6dde673ca4908e08324be8e95927d6e4f0
lsh <rrgks@naver.com>
2021-05-17 17:09:08 +0900

    leeseokho Select-Box-이용

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,17 @@
 "use strict";
+const select = document.getElementsByName("selectbox")[0];
+const button = document.getElementById("submit-btn");
+
+button.addEventListener("click", () => {
+    fetch('/api/selectbox/submit', {
+        method: 'post',
+        headers: {
+        'Content-type' : 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+            answer: select.value,
+        })
+      })
+      .then(res => res.json())
+      .then(res => result.innerHTML = res ? "정답" : "오답");
+});
