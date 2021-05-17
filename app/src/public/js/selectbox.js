fb045e90aeb596d3af807a13a269e4b3209e51ec
unknown <pho9902@naver.com>
2021-05-17 17:07:46 +0900

    parkhyunwoo : selectbox 풀이

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,20 @@
 "use strict";
+const submitBtn = document.querySelector('#submit-btn');
+const selectBox = document.querySelector('select');
+const result = document.getElementById("result");
+
+submitBtn.addEventListener('click', () => {
+    fetch('/api/selectbox/submit', {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json; charset=utf-8",
+        },
+        body : JSON.stringify({
+            answer : selectBox.value
+        })
+    })
+    .then((res) => res.json())
+    .then((isAnswer) => result.innerHTML = isAnswer ? "정답" : "오답");
+});
+
+console.log(2)
