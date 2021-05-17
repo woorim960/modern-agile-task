ff88c83776a8a10b4e03287f43ab08f43c89992b
minsoonki <alstnsrl98@gmail.com>
2021-05-17 17:08:17 +0900

    민순기 selectBox과제

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const output = document.getElementById('result');
+const button = document.getElementById('submit-btn');
+const select = document.getElementsByName('selectbox')[0];
+
+
+const request = (selectedValue) => {
+    const data = {
+        answer: selectedValue
+    };
+
+    fetch("/api/selectbox/submit", {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+        },
+        body : JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if(data) output.innerText = "정답";
+        else if (!data) output.innerText = "오답";
+    });
+} 
+
+button.addEventListener("click", () => {
+    const selectedValue = select.value;
+    request(selectedValue)
+});
+
+
+
+
+
+
+
