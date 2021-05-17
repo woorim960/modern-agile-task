79e390527cec09ec25c2b33c5ffbaf101d57b0f4
Bsfla <baebeomsu77@gmail.com>
2021-05-17 17:41:58 +0900

    배범수 제출합니다

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,23 @@
 "use strict";
+const selectBox = document.querySelector("select");
+const submitBtn = document.querySelector("#submit-btn");
+const result = document.querySelector("#result");
+
+submitBtn.addEventListener("click", onSubmit);
+
+function onSubmit(e) {
+    fetch('/api/selectbox/submit', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            answer: selectBox.value,
+        })
+    })
+    .then(res => res.json())
+    .then(isAnswer => {
+        result.innerHTML = isAnswer ?  '정답' : '오답';
+    });
+    
+}
