7fbaac897cdbeefc9d2456da9e61e4ce397c4568
seohyunsim <ssh4563@naver.com>
2021-05-18 17:51:53 +0900

    심서현 check-box

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,29 @@
-"use strict";
+const checkBox = document.querySelectorAll('#checkbox label input');
+const button = document.querySelector("#submit_btn");
+const span = document.querySelector("#result");
+
+
+function add() {
+    const boxes = [];
+    for (let i = 0; i < checkBox.length; i++) {
+        if (checkBox[i].checked)
+            boxes.push(checkBox[i].value);
+            console.log(boxes);
+    }
+    return boxes;
+}
+
+button.addEventListener("click", () => {
+    fetch("/api/checkbox/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+            answers : add()
+        }),
+      })
+      .then((res) => res.json())
+      .then((isAnswer) => span.innerHTML = isAnswer ? "정답" : "오답");
+    });
+
