576325d7b88608fcc1950cdaf1156b4e45fd90d0
minsoonki <alstnsrl98@gmail.com>
2021-05-18 14:27:41 +0900

    민순기 checkbox

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const button = document.querySelector("#submit_btn"),
+    output = document.querySelector("#result")
+    
+
+button.addEventListener("click", () => {
+    const checkedValue = [];
+
+    for (let i = 1; i < 6; i++) {
+        const checkbox = document.querySelector(`#checkbox > label:nth-child(${i}) > input[type=checkbox]`)
+        if(checkbox.checked) checkedValue.push(checkbox.value);
+    }  request(checkedValue);
+});
+
+const request = (checkedValue) => {
+
+    const data = {answers : checkedValue},
+    head = {'Content-Type' : 'application/json; charset=utf-8',}
+
+    const obj = {
+        method : 'POST',
+        headers : head,
+        body : JSON.stringify(data)
+    }
+    
+    fetch("/api/checkbox/submit", obj)
+        .then((res) => res.json())
+        .then((data) => {
+            data ? output.innerText = "정답" : output.innerText = "오답"
+        });
+} 
