85fa0d30243ec0e1a911d544bc86862b4872368f
Bsfla <baebeomsu77@gmail.com>
2021-05-18 14:32:56 +0900

    배범수 check box구현

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,38 @@
 "use strict";
+const checkBox = document.querySelectorAll("#checkbox label input");
+const submitBtn = document.querySelector("#submit_btn");
+const result = document.querySelector("#result");
+
+submitBtn.addEventListener("click", onSubmit);
+
+
+
+
+function onSubmit() {
+    const checkValue = {
+        answers : []
+    };
+    const { answers } = checkValue;
+
+   
+
+    checkBox.forEach(input => {
+        if (input.checked) answers.push(input.value);
+    });
+
+    fetch('/api/checkbox/submit', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkValue),
+    })
+    .then(res => res.json())
+    .then(isAnswer => {
+        result.innerHTML = isAnswer ?  '정답' : '오답';
+    });
+}
+
+
+    
+ 
