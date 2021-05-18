2980e6fcc7efc69967bc7cef82d51ccdedea4176
유준상 <junsang@yujunsangs-MacBook-Pro.local>
2021-05-18 14:20:11 +0900

    유준상 제출

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const checkBoxes = document.querySelectorAll('input');
+const submitButton = document.querySelector('#submit_btn')
+const resultSpan = document.querySelector('#result');
+
+submitButton.addEventListener('click', async () => {
+  const answers = Array.from(checkBoxes)
+  .filter((checkBox) => checkBox.checked)
+  .map((checkBox) => checkBox.value);
+
+  const response = await fetch('/api/checkbox/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ answers })
+  });
+  const result = await response.json();
+
+  resultSpan.textContent = (result) ? '정답' : '오답';
+});
