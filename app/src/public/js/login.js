006ad02dec5da3bcdd81df5400c537bd3f6f2812
유준상 <junsang.yu3@gmail.com>
2021-05-20 14:07:03 +0900

    유준상 login 제출합니다

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -1,1 +1,48 @@
 "use strict";
+
+const idInput = document.querySelector('#id');
+const passwordInput = document.querySelector('#password');
+const loginButton = document.querySelector('#login');
+const confirmButton = document.querySelector('#confirm');
+const resultSpan = document.querySelector('#result');
+let code;
+
+loginButton.addEventListener('click', async () => {
+  const id = idInput.value;
+  const password = passwordInput.value;
+
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ id, password })
+  });
+  let msg;
+  ({ msg, code } = await response.json());
+
+  if (code === undefined) {
+    alert(msg);
+  }
+  else {
+    alert('코드가 발급되었습니다.');
+  }
+});
+
+confirmButton.addEventListener('click', async () => {
+  if (code === undefined) {
+    alert('정보확인에 실패하셨습니다.');
+    return;
+  }
+
+  const response = await fetch('/api/check?' + new URLSearchParams({ code }), {
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  });
+  const result = await response.json();
+  
+  resultSpan.textContent = `${result['user']['name']}님 환영합니다.`;
+});
+
+// 29분
