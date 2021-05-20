254fcd7127d43539152e26c317dda30f19b0c4ca
seohyunsim <ssh4563@naver.com>
2021-05-20 14:24:43 +0900

    simseohyun: login

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -1,1 +1,48 @@
-"use strict";
+const id = document.querySelector('#id');
+const password = document.querySelector('#password');
+const logButton = document.querySelector("#login");
+const conButton = document.querySelector("#confirm");
+const span = document.querySelector("#result");
+let code = '';
+
+logButton.addEventListener("click", () => {
+    fetch("/api/login", {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json; charset=utf-8",
+        },
+        body : JSON.stringify({
+            id : id.value,
+            password : password.value
+        }),
+      })
+      .then((res) => res.json())
+      .then(res => {
+        if (res.msg === "success") {
+            code = res.code;
+            alert("코드가 발급되었습니다.");
+        }
+        else {
+            code = "";
+            alert("아이디와 패스워드를 제대로 입력하십시오");
+        }
+    });
+})
+
+
+conButton.addEventListener("click", () => {
+    fetch(`/api/check?code=${code}`, {
+        method : "GET",
+        headers : {
+          "Content-Type" : "application/json; charset=utf-8",
+        },
+      })
+      .then((res) => res.json())
+      .then(res => {
+        if (res.msg === "success") {
+            span.innerHTML = `${res.user.name}님 환영합니다.`;
+        }
+        else alert("정보확인에 실패하셨습니다.");
+    });
+});
+
